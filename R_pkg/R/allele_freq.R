038aa1611c4d18c07aeb94ac0488c52681625ab3
Guangchuan Ji <43041211+AI1013@users.noreply.github.com>
2018-12-16 13:20:45 -0500

    V1.02

--- R_pkg/R/allele_freq.R
+++ R_pkg/R/allele_freq.R
@@ -1,7 +1,16 @@
-#' @title Allele frequency Data.
+#' @title Allele frequency Data from the paper.
 #' @description A dataset containing the allele frequencies of Europe, America, Africa, Latino populations at 678 different loci.
 #' @format The first column is the name of 678 different loci, the third column includes the allele frequencies of four
 #' populations(Europe, America, Africa, Latino) of different alleles at this locus.
 #' @references Wang, S., Ray, N., Rojas, W., Parra, M. V., Bedoya, G., Gallo, C., ... & Camrena, B. (2008). 
 #' Geographic patterns of genome admixture in Latin American Mestizos. PLoS genetics, 4(3), e1000037.
 "allele.frequencies"
+
+#' @title Allele frequency Data created by us.
+#' @description A dataset containing the allele frequencies of Europe, America, Africa, Latino populations at 678 different loci. This 
+#' dataset is the same as the one from the paper.
+#' @format The first column is the name of 678 different loci, the third column includes the allele frequencies of four
+#' populations(Europe, America, Africa, Latino) of different alleles at this locus.
+#' @references Wang, S., Ray, N., Rojas, W., Parra, M. V., Bedoya, G., Gallo, C., ... & Camrena, B. (2008). 
+#' Geographic patterns of genome admixture in Latin American Mestizos. PLoS genetics, 4(3), e1000037.
+"allele.frequency"
